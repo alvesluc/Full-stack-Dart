c28521209626da03e710fc293c0801c46cb74219
Lucas Alves <l.alves001@gmail.com>
2022-07-15 20:54:29 -0300

    Finished WebSocket listener example

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -3,11 +3,14 @@ import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 // Configure routes.
 final _router = Router()
   ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
+  ..get('/echo/<message>', _echoHandler)
+  ..get('/ws', webSocketHandler(_webSocketHandler));
 
 Response _rootHandler(Request req) {
   return Response.ok('Hello, World!\n');
@@ -18,12 +21,20 @@ Response _echoHandler(Request request) {
   return Response.ok('$message\n');
 }
 
+void _webSocketHandler(WebSocketChannel webSocket) {
+  webSocket.stream.listen((message) {
+    webSocket.sink.add("Message via socket $message");
+  });
+}
+
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
   // Configure a pipeline that logs requests.
-  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  final handler = Pipeline() //
+      .addMiddleware(logRequests())
+      .addHandler(_router);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
